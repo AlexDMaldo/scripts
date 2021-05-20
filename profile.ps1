b5286171e56dc3ee1cbaa971bab9addfb731113b
Alex Maldonado <alexdmaldo@pm.me>
2021-05-19 19:10:27 -0500

    making scripts

---
+++ profile.ps1
@@ -0,0 +1,15 @@
+﻿$NTIdentity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$NTPrincipal = new-object Security.Principal.WindowsPrincipal $NTIdentity
+$IsAdmin = $NTPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+$hostversion="v$($Host.Version.Major).$($Host.Version.Minor)"
+$hostname = $host.Name
+
+IF ($IsAdmin)
+{
+        $host.ui.RawUI.WindowTitle = "ADMINISTRATOR: $hostname $hostversion"
+}
+Else
+{
+        $host.ui.RawUI.WindowTitle = "$hostname $hostversion"
+}
---
+++ sccm.ps1
@@ -0,0 +1,3 @@
+﻿WMIC /namespace:\\root\ccm path SMS_LookupMP
+WMIC /namespace:\\root\ccm path SMS_Client CALL GetAssignedSite
+$([WmiClass]"\\%COMPUTERNAME%\ROOT\ccm:SMS_Client").getassignedsite() | select sSiteCode
